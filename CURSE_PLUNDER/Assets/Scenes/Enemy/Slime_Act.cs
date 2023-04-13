b66637ee8ffe0a402348bb56ee3e03e752538b2e
Yamisaki <h1202016002@hiratagakuen.onmicrosoft.com>
2023-04-13 16:35:49 +0900

    作業途中プッシュ

--- CURSE_PLUNDER/Assets/Scenes/Enemy/Slime_Act.cs
+++ CURSE_PLUNDER/Assets/Scenes/Enemy/Slime_Act.cs
@@ -5,28 +5,36 @@ using UnityEngine;
 public class Slime_Act : MonoBehaviour
 {
     [SerializeField] GameObject TargetObj;
+    [SerializeField] GameObject HPBar;
 
     Vector2 TargetPos;  //追跡するターゲットの座標が入る
     Vector2 SlimePos;   //自分自身の座標
 
     private float   MOVE_SPEED  = 10.5f;    //移動速度
     private int     direction   = 0;  //アニメーターに向いている方向を教える為の変数
-    private int     Slime_HP    = 10;  //この敵のHP
+    [SerializeField] private int     Slime_HP    = 10;  //この敵のHP
+    private int     Slime_MaxHP;
     private float   rad;
 
     Rigidbody2D SlimeRB;  //この敵のリジッドボディ
     Animator SlimeAnim; //この敵のアニメーター
 
+    [SerializeField] float HP_per;
     void Start()
     {
         SlimeAnim = GetComponent<Animator>();
         SlimeRB = GetComponent<Rigidbody2D>();
-
+        Slime_MaxHP = Slime_HP;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //HPバーの更新
+        HP_per = (float)Slime_HP / (float)Slime_MaxHP;
+        HPBar.GetComponent<RectTransform>().sizeDelta = new Vector2(0.4f * HP_per, 0.04585f);
+
+
         //追跡するオブジェクトの座標情報を更新
         TargetPos = TargetObj.GetComponent<Transform>().position;
 
@@ -55,5 +63,14 @@ public class Slime_Act : MonoBehaviour
         //ターゲットのオブジェクトを追いかけるように移動
         SlimeRB.velocity = new Vector2((Mathf.Cos(rad) * MOVE_SPEED), -(Mathf.Sin(rad) * MOVE_SPEED));
 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Sword"))
+        {
+            Slime_HP--;
+            if (Slime_HP <= 0) Destroy(this.gameObject);
+        }
     }
 }
--- CURSE_PLUNDER/Assets/Scenes/Player/Script/PlayerSwordThrow.cs
+++ CURSE_PLUNDER/Assets/Scenes/Player/Script/PlayerSwordThrow.cs
@@ -6,27 +6,33 @@ using UnityEngine.UI;
 
 public class PlayerSwordThrow : MonoBehaviour
 {
-
-    [SerializeField] Sprite[] Sword_IMG = new Sprite[4];
-    [SerializeField] float MOVE_SPEED = 1000.0f;
+    [SerializeField] float MOVE_SPEED = 50.0f;
+    
     Rigidbody2D SwordRB;
     Vector2 Directions;
-    
+    Vector2 PlayerDir;
+
+    float rad;
     void Start()
     {
         SwordRB = GetComponent<Rigidbody2D>();
-        
-        
+
+
+        rad = PlayerControl.PLAYER_DIR_RAD * Mathf.Deg2Rad;
+        transform.rotation = new Quaternion(0, 0,-PlayerControl.PLAYER_DIR_RAD, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SwordRB.velocity = new Vector2(Mathf.Cos(rad) * MOVE_SPEED, -Mathf.Sin(rad) * MOVE_SPEED);
+
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Enemy")||collision.gameObject.CompareTag("Wall")){
+            Destroy(this.gameObject);
+		}
     }
 }
