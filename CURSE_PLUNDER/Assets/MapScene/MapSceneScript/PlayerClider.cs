8d8d909bac33d92a20ca8e428aa6832d01de32a9
oketanikouki <h1202016015@hiratagakuen.onmicrosoft.com>
2023-04-17 17:41:11 +0900

    UIの調整戻す必要はなし。
    UIの調整
    ステータスパネル再生成
    これからTIMEの作成に移る

--- CURSE_PLUNDER/Assets/MapScene/MapSceneScript/PlayerClider.cs
+++ CURSE_PLUNDER/Assets/MapScene/MapSceneScript/PlayerClider.cs
@@ -33,6 +33,18 @@ public class PlayerClider : MonoBehaviour
 
 
         }
+        
 
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            SceneManager.LoadScene("BattleScene");
+
+
+        }
+    }
+
 }
