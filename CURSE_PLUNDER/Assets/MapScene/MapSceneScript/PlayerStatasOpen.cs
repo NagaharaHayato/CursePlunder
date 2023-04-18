9a9cab4ec65968033fc7c8f1825f04e20385fd66
oketanikouki <h1202016015@hiratagakuen.onmicrosoft.com>
2023-04-18 16:29:18 +0900

    timerの設定完了
    今回やったこと
    UIの調整
    Timerの作成。
    次回やること
    Panelバグの修正
    洞窟ステージを凝る。
    他にあることを考えなければ

--- CURSE_PLUNDER/Assets/MapScene/MapSceneScript/PlayerStatasOpen.cs
+++ CURSE_PLUNDER/Assets/MapScene/MapSceneScript/PlayerStatasOpen.cs
@@ -10,11 +10,14 @@ public class PlayerStatasOpen : MonoBehaviour
     /// </summary>
     public GameObject WindowObject;
     public GameObject WindowObject2;
+    public GameObject WindowObject3;
     //オープン用メッセージウィンドウは表示扱い
     bool Wenabled = true;
     
     //ステータスウィンドウは非表示扱い
     bool Wenabled2 = false;
+
+    //bool Wenabled3 = false;
 
 
     private void FixedUpdate()
@@ -24,6 +27,9 @@ public class PlayerStatasOpen : MonoBehaviour
         //ウィンドウ2をセット
         WindowObject2.SetActive(Wenabled2);
 
+        //ウィンドウ2をセット
+        //WindowObject3.SetActive(Wenabled3);
+
     }
     private void Update()
     {
@@ -32,12 +38,14 @@ public class PlayerStatasOpen : MonoBehaviour
         {
             Wenabled = false;
             Wenabled2 = true;
+           // Wenabled3 = true;
         }
         //閉じる
         if(Input.GetKeyDown(KeyCode.D))
         {
             Wenabled = true;
             Wenabled2 = false;
+           
         }
 
 
