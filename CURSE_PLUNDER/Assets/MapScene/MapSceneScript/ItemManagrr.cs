400ce97dd694fdb202d5ad1c8fcb8e25b58a8196
oketanikouki <h1202016015@hiratagakuen.onmicrosoft.com>
2023-04-24 03:00:07 +0900

    managerclass作詞
    アイテムmanagerを作る

--- CURSE_PLUNDER/Assets/MapScene/MapSceneScript/ItemManagrr.cs
+++ CURSE_PLUNDER/Assets/MapScene/MapSceneScript/ItemManagrr.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEditorInternal;
+using UnityEditor.Search;
 
 public class ItemData
 {
@@ -29,15 +31,46 @@ public class ItemData
 
 public class ItemManagrr : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private List<Item> _item;//アイテムデータベース
+
+    private List<ItemData> _PlayerItemDataList = new List<ItemData>();//プレイヤーの所持アイテム
+
+    private void Awake()
     {
-        
+        LoadItemSourceData();  
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LoadItemSourceData()
     {
-        
+        _item = Resources.LoadAll("ScriptableObject", typeof(Item)).Cast<Item>().ToList();
     }
+    //アイテムをロードする
+    public Item GetItem(string id)
+    {
+        foreach(var sourceData in _item)
+        {
+            if(sourceData.id==id)
+            {
+                return sourceData;
+            }
+        }
+        return null;
+    }
+
+    //アイテムを取得
+    public void CountItem(string itemid,int count)
+    {
+        for(int i=0;i<_PlayerItemDataList.Count;i++)
+        {
+            //ID一致でカウント
+            if (_PlayerItemDataList[i].id==itemid)
+            {
+                _PlayerItemDataList[i].CountUp(count);
+                break;
+            }
+        }
+        ItemData itemData = new ItemData(itemid, count);
+        _PlayerItemDataList.Add(itemData);
+    }
+
 }
