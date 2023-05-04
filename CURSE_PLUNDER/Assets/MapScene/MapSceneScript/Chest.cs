cb0d7f2d364f747421b420120a8eea637ab7beb1
oketanikouki <h1202016015@hiratagakuen.onmicrosoft.com>
2023-05-05 01:35:47 +0900

    アイテム実装中

--- CURSE_PLUNDER/Assets/MapScene/MapSceneScript/Chest.cs
+++ CURSE_PLUNDER/Assets/MapScene/MapSceneScript/Chest.cs
@@ -1,8 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Chest : MonoBehaviour
 {
-   
+    [SerializeField] private Item item;
+    [SerializeField] private Sprite openImage;
+    [SerializeField] private Sprite closeImage;
+
+    private bool isOpened = false;
+    private SpriteRenderer spriteRenderer;
+
+    public bool IsOpened
+    {
+        get => isOpened; set
+        {
+            isOpened = value;
+            spriteRenderer.sprite = value ? openImage : closeImage;
+        }
+    }
+    //protected override string
+
+    private void Start()
+    {
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+    }
 }
