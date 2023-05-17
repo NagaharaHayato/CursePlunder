b560d380ad4b5c0ec646d11c80467cc249e29157
NagaharaHayato <72540088+NagaharaHayato@users.noreply.github.com>
2023-05-17 15:20:30 +0900

    ボス専用戦闘シーンの追加

--- CURSE_PLUNDER/Assets/Scenes/BattleScene/UIManager.cs
+++ CURSE_PLUNDER/Assets/Scenes/BattleScene/UIManager.cs
@@ -11,29 +11,34 @@ public class UIManager : MonoBehaviour
     [SerializeField] GameObject TimeoverUI;
     [SerializeField] GameObject CommandSelectUI;
     [SerializeField] GameObject TimeLimitUI;
+    [SerializeField] GameObject BossDefeat_Fade;
 
     [SerializeField] TextMeshProUGUI TimeCount;
+
+    Animator BDF_Animator;
 
     public static bool isWin = false;
     void Start()
     {
-        
+        BDF_Animator = BossDefeat_Fade.GetComponent<Animator>();   
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ((GameObject.FindGameObjectsWithTag("Enemy").Length <= 0)) {
-            
-            //VictoryUI.SetActive(true);
-            //isWin = true;
-        }
-		Timer.countdownSecound -= Time.deltaTime;
-		var span = new TimeSpan(0, 0, (int)Timer.countdownSecound);
+        if (BossUroboros.BossHP > 0) Timer.countdownSecound -= Time.deltaTime;
+        if (BossUroboros.BossHP <= 0) BossDefeat_Fade.SetActive(true);
+        
+        var span = new TimeSpan(0, 0, (int)Timer.countdownSecound);
         TimeCount.text = span.ToString(@"mm\:ss");
 
         if (Timer.countdownSecound <= 0) TimeoverUI.SetActive(true);
 
         CommandSelectUI.SetActive(PlayerControl.cmdselect_dialog);
+
+        if (BossDefeat_Fade.activeInHierarchy && BDF_Animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("ClearScene");
+        }
     }
 }
